5a1841e03419ad23da0cb0221d820536ef8f4729
Abhinav Duvvuri <dvabhinav31@gmail.com>
2024-02-20 19:50:08 -0800

    Added basic functions and interfaces for model.py and visualization generator

---
+++ scripts/visualization_generator.py
@@ -0,0 +1,26 @@
+class visualizationGenerator:
+    def __init__(self, seasonFrom=1, episodeFrom=1  , seasonTo=1, episodeTo=1):
+        self.episodeFrom = episodeFrom
+        self.episodeTo = episodeTo
+        self.seasonFrom = seasonFrom
+        self.seasonTo = seasonTo
+        
+    def preProcessData(self):
+        processedString = ''
+        return processedString
+    
+    
+    def wordCloud(self):
+        return ''
+    
+    def generateSingleSentiment(self):
+        sentiment = 0
+        return sentiment
+    
+    def generateSentiment(self):
+        listofSentiments = []
+        return listofSentiments
+    
+
+    def sentimentAnalysisVisualization(self):
+        return ''
