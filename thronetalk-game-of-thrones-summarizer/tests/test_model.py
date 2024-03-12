48d47595281b7d2bf14e7a2b5ab78f5f6e2f9a72
Raagul N <raaguln@uw.edu>
2024-03-12 01:40:33 -0700

    mocking summary function

--- thronetalk-game-of-thrones-summarizer/tests/test_model.py
+++ thronetalk-game-of-thrones-summarizer/tests/test_model.py
@@ -3,10 +3,14 @@ Module that tests `scripts/model.py`. Makes use of `unittest` module.
 Consists of smoke tests and edge tests.
 '''
 import unittest
-# from unittest.mock import patch
+from unittest.mock import patch
 from utils import Model
+from . import mock_constants
 
 class TestModel(unittest.TestCase):
+    # def setUp(self):
+    #     self.model = Model(1, 1, 1, 1)
+
     '''Test suite for `scripts/model.py`'''
     # Smoke tests
     # @patch('scripts.visualization_generator.pd.read_csv',
@@ -53,6 +57,19 @@ class TestModel(unittest.TestCase):
             episode {str(episode_from)} to season {str(season_to)}
             episode {str(episode_to)} in 300 words.'''.replace(" ", "")
         self.assertEqual(expected_message, computed_message)
+
+    @patch('utils.model.Model.azure_api_call')
+    def test_summarize(self, mock_summarize):
+        '''Test for summarize function'''
+        mock_summarize.return_value = mock_constants.CHAT_COMPLETIONS_MOCK_RESPONSE
+        model = Model(1,1,1,2)
+
+        # Assert that azure_api_call was called with the correct prompt
+        message_text = model.create_summarizer_input()
+        mock_summarize.assert_called_once_with(message_text)
+
+        summary = model.summarize()
+        self.assertEqual(summary, mock_constants.CHAT_COMPLETIONS_MOCK_RESPONSE)
 
 if __name__ == "__main__":
     unittest.main()
